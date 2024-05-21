8d4f994a18ac469ac593ddba178b6750a6f4034c
smcgb <smcgb1844@gmail.com>
2024-05-21 14:16:25 -0700

    Chore: completed 03 tests

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,12 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let reversedString = '';
+    
+    string = string.toString(); // convert to string if not already
+    
+    // https://www.freecodecamp.org/news/how-to-reverse-a-string-in-javascript-in-3-different-ways-75e4763c68cb/
+    reversedString = string.split('').reverse().join('');
+
+    return reversedString;
 
 };
 
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, num) {
+    
+    let index = array.indexOf(num);
+
+    if (index > -1) {
+        array.splice(index, 1); 
+      };
+
+    return array;
 
 };
+
+console.log(removeFromArray([1,2,3,4], 3))
 
 // Do not edit below this line
 module.exports = removeFromArray;
