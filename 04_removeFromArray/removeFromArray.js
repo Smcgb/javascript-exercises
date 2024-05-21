1d788744c674a3cda61eb955130326658d4960aa
smcgb <smcgb1844@gmail.com>
2024-05-21 15:21:42 -0700

    chore: completed lesson 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,16 +1,8 @@
-const removeFromArray = function(array, num) {
+const removeFromArray = function(array, ...args) {
+
+    return array.filter(x => !args.includes(x))
     
-    let index = array.indexOf(num);
-
-    if (index > -1) {
-        array.splice(index, 1); 
-      };
-
-    return array;
-
 };
-
-console.log(removeFromArray([1,2,3,4], 3))
 
 // Do not edit below this line
 module.exports = removeFromArray;
