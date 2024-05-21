d784a6dc826facf1e082c7d5ba2330c69a10814b
smcgb <smcgb1844@gmail.com>
2024-05-21 16:30:53 -0700

    chore: compelted lesson 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    
+    if (a < 0 || b < 0 || typeof(a) !== 'number' || typeof(b) !== 'number') {
+        return 'ERROR';
+    };
+    
+    let summation = 0;
+    let min = a;
+    let max = b;
 
+    if (a > b) {
+        min = b;
+        max = a;
+    };
+
+    for (i = min; i <= max; i++) {
+        summation += i;
+    };
+    
+
+    return summation;
 };
 
 // Do not edit below this line
