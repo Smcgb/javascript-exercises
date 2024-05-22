6d8d3597dde2cc02e9019a989305c134ae9f8e28
smcgb <smcgb1844@gmail.com>
2024-05-21 17:59:43 -0700

    chore: cursed completion of 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+
+  return parseFloat(((Math.round(100 * (temp-32) * 5 / 9)) / 100).toFixed(1));
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+
+  return parseFloat(((Math.round(100 * ((temp * 9 / 5) + 32))) / 100).toFixed(1));
+
 };
 
 // Do not edit below this line
